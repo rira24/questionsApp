7722253f2501f2b3359f952087cf33b94b6c4d38
Rithika <rithu427@gmail.com>
2023-07-14 09:35:11 -0500

    Initial Commit

---
+++ questionsApp/questionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionsAppApp.swift
+//  questionsApp
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct questionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
